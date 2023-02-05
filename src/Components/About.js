8a78fede1fb62fe52acd4cdc9cea3b90d9fc95fc
Vhickyy <104204584+Vhickyy@users.noreply.github.com>
2023-02-05 10:52:22 +0100

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <div className='w-[90%] mx-auto grid gap-[2rem]'>
             <h1 className='text-yellow-400 text-4xl font-bold tracking-wide'>Hello, I'm Kelechi Okorie.</h1>
             <div className='grid gap-[1rem] text-xl text-yellow-200'>
-                <p>Kelechi A. Okorie is a Canva Creative Designer with over two years of experience and has taught a lot of people are to create awesome designs and land their dream job.</p>
+                <p>Kelechi A. Okorie is a Canva Creative Designer with over two years of experience and has taught a lot of people how to create awesome designs and land their dream job.</p>
                 <p>As a business owner, whether online or offline, having a google my business profile will help make ypur business.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo odit, ex quia repudiandae optio incidunt iusto quaerat numquam molestiae harum?Lorem ipsum dolor sit amet consectetur</p>
             </div>
